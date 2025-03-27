8471aa76c2568c8ecb9477cbb228f8d6edfe4e95
Emeka Pascal <emekapascal11@gmail.com>
2025-03-27 08:15:46 +0100

    max sum average 1

---
+++ slidingwindow_fixedsize/maximum_subarray_1.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+// Code
+// Testcase
+// Test Result
+// Test Result
+// 643. Maximum Average Subarray I
+// Easy
+// Topics
+// Companies
+// You are given an integer array nums consisting of n elements, and an integer k.
+
+// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+// Example 1:
+
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+// Example 2:
+
+// Input: nums = [5], k = 1
+// Output: 5.00000
+
+// Constraints:
+
+// n == nums.length
+// 1 <= k <= n <= 105
+// -104 <= nums[i] <= 104
+
+func findMaxAverage(nums []int, k int) float64 {
+	maxSum := 0
+	currentSum := 0
+	for index, num := range nums {
+		currentSum += num
+		if index >= k {
+			currentSum -= nums[index-k]
+		}
+		if currentSum > maxSum {
+			maxSum = currentSum
+		}
+	}
+	return float64(maxSum) / float64(k)
+}
+
+func main() {
+	fmt.Println(findMaxAverage([]int{1, 12, -5, -6, 50, 3}, 4))
+	fmt.Println(findMaxAverage([]int{5}, 1))
+}
