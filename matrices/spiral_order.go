4150b3a4fb82d4d2482f2add3d59a39065d8dadf
Emeka Pascal <emekapascal11@gmail.com>
2025-04-06 16:48:13 +0100

    matrices 2 d array

---
+++ matrices/spiral_order.go
@@ -0,0 +1,48 @@
+package main
+
+// Description
+// Editorial
+// Editorial
+// Solutions
+// Solutions
+// Submissions
+// Submissions
+// Code
+// Testcase
+// Test Result
+// Test Result
+// 54. Spiral Matrix
+// Medium
+// Topics
+// Companies
+// Hint
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+// Example 1:
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [1,2,3,6,9,8,7,4,5]
+// Example 2:
+
+// Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+// Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 10
+// -100 <= matrix[i][j] <= 100
+// Accepted
+// 1.8M
+// Submissions
+// 3.5M
+// Acceptance Rate
+// 53.3%
+// Topics
+// Companies
+// Hint 1
+// Hint 2
+// Hint 3
+// Similar Questions
+// Discussion (219)
